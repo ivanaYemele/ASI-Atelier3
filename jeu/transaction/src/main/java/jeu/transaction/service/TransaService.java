d1a06ca137d4d410f605febe2634f92baf57779f
Absa <absa.so@hotmail.fr>
2021-05-21 08:51:43 +0200

    ajout des fichiers

--- jeu/transaction/src/main/java/jeu/transaction/service/TransaService.java
+++ jeu/transaction/src/main/java/jeu/transaction/service/TransaService.java
@@ -1,5 +1,43 @@
 package jeu.transaction.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class TransaService {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private CardService cardService;
+
+	//Le joeur peut acheter sa carte
+	public void buyCard(User u, Card card) {
+		double usrmoney = u.getMoney();
+		double cardprice = card.getPrice();
+		//on réalise la transaction
+		double achat = usrmoney - cardprice;
+		
+		//on met à jour la somme d'argent restante du joueur
+		u.setMoney(achat);
+		userService.updateMoney(u);
+		
+		
+	}
+
+	public void SellCard(User u, Card card) {
+		double usrmoney = u.getMoney();
+		double cardprice = card.getPrice();
+		double marge = 0.5;
+		//on réalise la transaction
+		double vente = usrmoney + cardprice + marge;
+		
+		//on met à jour la somme d'argent du joueur
+		u.setMoney(vente);	
+		userService.updateMoney(u);
+	}
 
 }
--- jeu/user/src/main/java/jeu/user/repository/UserRepository.java
+++ jeu/user/src/main/java/jeu/user/repository/UserRepository.java
@@ -1,5 +1,18 @@
 package jeu.user.repository;
 
-public interface UserRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	public Optional<User> findByName(String name);
+
+	public Optional<User> findByNameAndMdp(String name, String mdp);
 
 }
